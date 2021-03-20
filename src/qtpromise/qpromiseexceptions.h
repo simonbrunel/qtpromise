461f09bef8547d8696f909488afa31b399b9c78d
Dmitriy Purgin <dpurgin@gmail.com>
2021-03-20 09:25:41 +0100

    Remove QPromiseError; fix include order to compile on GCC 9 (#45)
    Co-authored-by: Ryan Henderson <the.unkle.george@gmail.com>

--- src/qtpromise/qpromiseexceptions.h
+++ src/qtpromise/qpromiseexceptions.h
@@ -8,7 +8,6 @@
 #ifndef QTPROMISE_QPROMISEEXCEPTIONS_H
 #define QTPROMISE_QPROMISEEXCEPTIONS_H
 
-#include "qpromise_p.h"
 #include "qpromiseglobal.h"
 
 #include <QtCore/QException>
@@ -64,12 +63,6 @@ public:
         return new QPromiseUndefinedException{*this};
     }
 };
-
-// QPromiseError is provided for backward compatibility and will be
-// removed in the next major version: it wasn't intended to be used
-// directly and thus should not be part of the public API.
-// TODO Remove QPromiseError at version 1.0
-using QPromiseError = QtPromisePrivate::PromiseError;
 
 } // namespace QtPromise
 
